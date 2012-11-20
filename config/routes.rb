ac8a2bb7b85e4093be0029f0280b8e9a8f48f293
emclab <emclab2011@gmail.com>
2012-11-20 17:59:30 -0600

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TestItemx::Engine.routes.draw do
 
-  mount Authentify::Engine => "/authentify"  #, :as => "authentify_engine"
+  #mount Authentify::Engine => "/authentify"  #, :as => "authentify_engine"
 
   resources :standards
   resources :test_items
